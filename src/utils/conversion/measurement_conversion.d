9afb12ece3b5b3fc191f4950e35fff2852b6fb6f
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2018-12-03 16:37:59 -0200

    Fix cleaning conversion objects

--- src/utils/conversion/measurement_conversion.d
+++
@@ -1,3 +0,0 @@
-utils/conversion/measurement_conversion.o: \
- utils/conversion/measurement_conversion.c \
- utils/conversion/../../../include/utils/conversion/measurement_conversion.h
--- src/utils/conversion/types_conversion.d
+++
@@ -1,2 +0,0 @@
-utils/conversion/types_conversion.o: utils/conversion/types_conversion.c \
- utils/conversion/../../../include/utils/conversion/types_conversion.h
